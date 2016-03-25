11881d2b25ba0f1fb3379e9fdffae6186e111fe4
dimalakh <dmytrolakh@gmail.com>
2016-03-25 16:44:05 +0200

    initial commit

--- wordGame/game.py
+++ wordGame/game.py
@@ -1,16 +1,8 @@
 from dictionary import *
 from sys import exit
+import time
+import random
 import os
-
-wordUsed = ["lam"]
-scoreStorage = [0]
-wordBot = dictionary
-originalDict = dictionary
-
-os.system('clear')
-
-print('Hello it is word game. \nType word-exit to close the game. \n\nRules: \n  1.Your word must start from letter which is the last letter of previous word.\n  2.If you type used word you will lose.')
-
 
 def wordSelector():
   i = -1
@@ -21,47 +13,146 @@ def wordSelector():
       print(wordBot[i])
       wordBot.pop(i)
       break
+
+def countrySelector():
+  selectedCountry = random.choice(wordBot)  
+  if selectedCountry in wordUsed:
+    countrySelector()
+  else:
+      wordUsed.append(selectedCountry)
+      print(selectedCountry)
   
 def score(wordScore):
-     scoreStorage.append(len(wordScore)*10)
-     return (sum(scoreStorage))
+  scoreStorage.append(len(wordScore)*10)
+  return (sum(scoreStorage))
+
+def header():
+  os.system('clear') 
+  print(gameName.upper()+'''             Type Q to close the game.\n___________________________________________________________________________''')
 
 
 
-for index in range(len(wordBot)): 
-  inpt = raw_input()
-  
-  if inpt in wordUsed:
-    print('\n\n  You lose!\n  Youre score:' + str(globalScore) + '\n  This word is already used!Do you want to start new game?Y/N')
-    controlInpt = raw_input()
-    
-    if controlInpt == 'Y':
-      print('I dont know how to do it')
-    else:
-      exit()  
-
-
-  elif inpt =='word-exit':
-    exit()
-
-
+def endGame():
+  print('''\n  ____________________________________
+    \n   You lose!                         
+    \n   Youre score:''' + str(globalScore) + '''         
+    \n   Do you want to start new game?Y/N 
+    \n  ____________________________________''')
+  controlInpt = raw_input()
+  if controlInpt == 'Y' or controlInpt == 'y':
+    execfile('game.py')
   else:
-    wordStorage = wordUsed[-1]
+    exit()     
 
-    if inpt in originalDict:
-      if wordStorage.endswith(inpt[0]):
-        wordUsed.append(inpt)
-        wordSelector()
-        globalScore = score(inpt)
+os.system('clear')
+
+print('''Hello it is word game.
+ \n\nChoose game type:
+ \n Press 1 - to play simple words game\n ______________________________
+ \n Press 2 - to play countries game\n ______________________________
+ \n Press 3 - to play cities game\n ______________________________
+ \n Press 4 - to play rivers game\n ______________________________''')
+
+endCounter = 0
+menuInpt = raw_input()
+
+if menuInpt == '2':
+  wordUsed = ["a"]
+  gameName = 'Countries'
+  scoreStorage = [0]
+  wordBot = countriesDict
+  originalDict = countriesDict
+
+  os.system('clear')
+  print('''Rules: \n1.Type random name of country.\n2.If you type used country name  you will lose.\n\nWait 10 seconds.''')
+  time.sleep(10)
+  os.system('clear')
+  header()
+
+  for index in range(len(wordBot)): 
+      inpt = raw_input()
+      
+      if inpt in wordUsed:
+        endGame()
+      elif inpt =='q' or inpt =='Q':
+        exit()
+      elif inpt == 'a' or inpt =='A':
+        countryAdvice()  
       else:
-        print('First letter must be '+wordStorage[-1])
+        if inpt in originalDict:
+            wordUsed.append(inpt)
+            countrySelector()
+            globalScore = score(inpt)
+        elif len(wordBot) == 0:
+          print('You win!') 
+        else:
+          print('it isnt word')    
+          endCounter = endCounter + 1
+          if endCounter > 5:
+            endGame()
+
+  print('You win!') 
 
 
-    elif len(wordBot) == 0:
-      print('You win!') 
-      
+elif menuInpt == '3':
+  gameName = 'Cities'
+  wordUsed = ["a"]
+  scoreStorage = [0]
+  wordBot = dictionary
+  originalDict = dictionary 
+elif menuInpt == '4':
+  gameName = 'Rivers'
+  wordUsed = ["a"]
+  scoreStorage = [0]
+  wordBot = dictionary
+  originalDict = dictionary 
+else:
+  wordUsed = ["a"]
+  gameName = 'Words'
+  scoreStorage = [0]
+  wordBot = dictionary
+  originalDict = dictionary 
 
+  os.system('clear')
+  print('''Rules: \n1.Your word must start from letter which is the last letter of previous word.\n2.If you type used word you will lose.\n\nWait 10 seconds.''')
+  time.sleep(10)
+  os.system('clear')
+  header()
+
+  for index in range(len(wordBot)): 
+    inpt = raw_input()
+    
+    if inpt in wordUsed:
+      endGame()  
+    elif inpt =='q' or inpt =='Q':
+      exit()
     else:
-      print('it isnt word') 
-      
-print('You win!') 
+      wordStorage = wordUsed[-1]
+      if inpt in originalDict:
+        if wordStorage.endswith(inpt[0]) and len(wordUsed)>1:
+          wordUsed.append(inpt)
+          wordSelector()
+          globalScore = score(inpt)
+        elif len(wordUsed)==1:
+          wordUsed.append(inpt)
+          wordSelector()
+          globalScore = score(inpt)
+        else:
+          print('First letter must be '+wordStorage[-1])
+      elif len(wordBot) == 0:
+        print('You win!') 
+      else:
+        print('it isnt word') 
+        endCounter = endCounter + 1
+        if endCounter > 5:
+          endGame()
+
+  print('You win!') 
+
+
+
+
+
+
+
+
